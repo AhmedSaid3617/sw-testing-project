a3447e965cbe8793e247b249b647d098236f2a24
AbdElRahman Soliman <AbdElRahman7987@gmail.com>
2025-10-21 15:08:47 +0300

    Implemented the Recommender function

--- src/main/java/com/example/Recommender.java
+++ src/main/java/com/example/Recommender.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Provides movie recommendation functionalities.
  */
@@ -23,7 +26,30 @@ public class Recommender {
      *
      * @param user The user for whom to generate recommendations.
      */
-    public void recommendMovies(User user) {
-        // Recommendation logic to be implemented
+    public List<Movie> recommendMovies(User user) {
+        // Get the list of movies the user likes
+        List<Movie> likedMovies = user.getLikedMovies();
+        // Get List of liked genres
+        List<String> likedGenres = new ArrayList<>();
+        for (Movie movie : likedMovies) {
+            List<String> genres = movie.getGenres();
+            for (String genre : genres) {
+                if (!likedGenres.contains(genre)) {
+                    likedGenres.add(genre);
+                }
+            }
+        }
+        // Recommend movies based on liked genres and exclude already liked movies
+        List<Movie> recommendations = new ArrayList<>();
+        for (Movie movie : data.getMovies()) {
+            if (!likedMovies.contains(movie)) continue;
+            for (String genre : movie.getGenres()) {
+                if (likedGenres.contains(genre)) {
+                    recommendations.add(movie);
+                    break;
+                }
+            }
+        }
+        return recommendations;
     }
 }
